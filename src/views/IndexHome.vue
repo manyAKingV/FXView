48befbe9e4c7f1bfffec25311bb467070f0124ac
吴宇佳 <wuyujia@wuyujiadeMBP.lan>
2025-06-13 10:55:38 +0800

    feat: landscape页面开发

--- src/views/IndexHome.vue
+++ src/views/IndexHome.vue
@@ -1,30 +1,105 @@
 <template>
   <FitScreen mode="full" :height="1080" :width="1920">
     <div class="container">
-      <div class="firm-log" v-for="(data, index) in showData" :key="index">
-        <div class="firm-title">{{ data.name }}</div>
-        <div class="firm-list-container">
-          <div
-            class="firm-list"
-            v-for="(item, index) in data.subcategories"
-            :key="index"
-            :class="{
-              'firm-list-one': index === 0,
-              'firm-list-two': index === 1,
-              'firm-list-three': index === 2,
-              'firm-list-four': index === 3,
-              'firm-list-five': index === 4,
-            }"
-          >
-            <div class="firm-type">{{ item.name }}</div>
-            <div class="firm-list-logo">
-              <img
-                :src="getLogoUrl(it.logo)"
-                class="firm-list-logo-item-img"
-                v-for="(it, ind) in item.items"
-                :key="ind"
-                @error="handleImageError"
-              />
+      <div class="container-title">AI全景图</div>
+      <div class="container-content">
+        <div class="container-content-box">
+          <div class="container-content-box-title">
+            {{ data.landscape.category.name }}
+          </div>
+          <div class="container-content-box-show">
+            <div
+              class="container-content-box-show-item"
+              :class="{
+                'container-content-box-show-item-last':
+                  index % 4 === 0 && index != 0,
+              }"
+              v-for="(item, index) in showData"
+              :key="index"
+            >
+              <div class="container-content-box-show-item-title">
+                {{ item.name }}
+              </div>
+              <div class="container-content-box-show-item-content">
+                <div
+                  class="img-show"
+                  v-for="(it, ind) in item.items"
+                  :key="ind"
+                  @click="showCompanyInfo(ind, index, it)"
+                >
+                  <img
+                    :src="require(`@/assets/logos/${it.logo}`)"
+                    class="img-item"
+                  />
+                  <div
+                    class="img-detail"
+                    v-if="currentIndex === ind && currentType === index"
+                  >
+                    <div class="img-detail-title">
+                      <img
+                        class="img-detail-title-img"
+                        :src="require(`@/assets/logos/${showDetailInfo?.logo}`)"
+                      />
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="28"
+                        height="28"
+                        viewBox="0 0 28 28"
+                        fill="none"
+                        class="img-close"
+                        @click="handleClose"
+                      >
+                        <path
+                          d="M7.63635 7.63635L20.3636 20.3636"
+                          stroke="#666666"
+                          stroke-width="2"
+                        />
+                        <path
+                          d="M20.3636 7.63635L7.63635 20.3636"
+                          stroke="#666666"
+                          stroke-width="2"
+                        />
+                      </svg>
+                    </div>
+                    <div class="img-detail-title-text">
+                      {{ showDetailInfo?.name }}
+                    </div>
+                    <div class="img-detail-title-country">中国</div>
+                    <div class="img-detail-title-desc">
+                      {{ showDetailInfo?.description }}
+                    </div>
+                    <div class="img-detail-bottom-desc">
+                      <div class="img-detail-bottom-desc-one">
+                        创立于<span class="img-detail-bottom-desc-span">{{
+                          handleTime()
+                        }}</span>
+                      </div>
+                      <div class="img-detail-bottom-desc-one">
+                        文本展示<span class="img-detail-bottom-desc-span"
+                          >xxx</span
+                        >
+                      </div>
+                      <div
+                        class="img-detail-bottom-desc-web"
+                        @click="pathStamp"
+                      >
+                        <span class="web-text">网站</span>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          viewBox="0 0 20 20"
+                          fill="none"
+                        >
+                          <path d="M15 9L14.9998 5H10.9998" stroke="black" />
+                          <path d="M15.091 5L11.2046 8.88642" stroke="black" />
+                          <path d="M8.33333 5H5V15H15V11.6667" stroke="black" />
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -35,36 +110,51 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import FitScreen from "@fit-screen/vue";
-import jsyaml from 'js-yaml'
-const showData = ref([])
-
+import jsyaml from "js-yaml";
+const currentIndex = ref(null);
+const currentType = ref(null);
+const showDetailInfo = ref(false);
+const showData = ref([]);
 onMounted(async () => {
-  const response = await fetch('/static/data.yaml')
-  const yamlText = await response.text()
-  const parsedData = jsyaml.load(yamlText)
-  console.log(parsedData)
-  showData.value = parsedData.landscape
-})
+  const response = await fetch("/static/data.yaml");
+  const yamlText = await response.text();
+  const parsedData = jsyaml.load(yamlText);
+  showData.value = parsedData.landscape.category.subcategories;
+});
 
 const getLogoUrl = (logoName) => {
   try {
     return require(`@/assets/logos/${logoName}`);
   } catch (e) {
-    return ''; // 返回空字符串表示找不到图片
+    return ""; // 返回空字符串表示找不到图片
   }
 };
 
 // 处理图片加载错误
 const handleImageError = (event) => {
-  event.target.style.display = 'none';
+  event.target.style.display = "none";
 };
-
+const showCompanyInfo = (ind, index, it) => {
+  currentIndex.value = ind;
+  currentType.value = index;
+  showDetailInfo.value = it;
+};
+const handleClose = () => {
+  currentIndex.value = null;
+  currentType.value = null;
+};
+const pathStamp = () => {
+  window.location.href = showDetailInfo.value?.homepage_url;
+};
+const handleTime = () => {
+  return showDetailInfo.value?.description?.split(" ")?.[1].split("-")?.[0];
+};
 </script>
 <style scoped>
 .container {
   width: 100%;
   height: 100%;
-  padding: 30px;
+  box-sizing: border-box;
 }
 .firm-log {
   background: #e5fbf7;
@@ -75,57 +165,192 @@ const handleImageError = (event) => {
   min-height: 200px;
   border: 1px dotted #63be90;
   border-radius: 8px;
-  display: flex;
-  margin-bottom: 8px;
-  padding: 6px 4px;
 }
-.firm-type {
-  font-size: 12px;
-  color: #4a4849;
-  width: 80px;
+.container-title {
+  width: 100%;
+  height: 80px;
+  flex-shrink: 0;
+  background: #fff;
+  box-shadow: 0px 4px 8px 0px rgba(0, 0, 0, 0.05);
+  color: #000;
+  font-family: "PingFang HK";
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 2px;
+  padding-left: 42px;
   display: flex;
-  justify-content: center;
   align-items: center;
 }
-.firm-list-logo {
-  width: calc(100% - 80px);
-  background: #ffffff;
-  border-radius: 8px;
+.container-content {
+  width: 100%;
+  padding: 20px;
 }
-.firm-list-logo-item {
-  padding: 0 10px;
+.container-content-box-title {
+  width: 100%;
+  height: 60px;
+  flex-shrink: 0;
+  background: #fe8fb9;
+  padding-left: 40px;
+  display: flex;
+  align-items: center;
+  color: #000;
+  text-align: center;
+  font-family: "PingFang HK";
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  letter-spacing: 2px;
 }
-.firm-list-logo-item-img {
-  max-width: 120px;
-  height: 100px;
-  object-fit: contain;
-  padding: 0 10px;
-}
-.firm-list-logo-item-text {
-  font-size: 10px;
-  color: #423f3f;
-}
-.firm-title {
-  color: #63be90;
-  margin: 12px 6px;
-}
-.firm-list-one {
-  width: 50%;
-}
-.firm-list-two {
-  width: 25%;
-}
-.firm-list-three {
-  width: 25%;
-}
-.firm-list-four {
-  width: 55%;
-}
-.firm-list-five {
-  width: 45%;
-}
-.firm-list-container {
+.container-content-box-show {
+  padding: 20px;
+  background: #fff0f6;
+  min-height: 200px;
   display: flex;
   flex-wrap: wrap;
+}
+.container-content-box-show-item {
+  margin-right: 6px;
+  margin-top: 10px;
+  width: 362px;
+  box-sizing: border-box;
+}
+.container-content-box-show-item-last {
+  margin-right: 0px;
+}
+.container-content-box-show-item-title {
+  height: 40px;
+  flex-shrink: 0;
+  border: 1px solid rgba(0, 0, 0, 0.05);
+  background: #fff;
+  color: #000;
+  font-family: "PingFang HK";
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 1.4px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.container-content-box-show-item-content {
+  padding: 20px;
+  background: #fff;
+  height: 164px;
+  display: flex;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  /* display: flex;
+  flex-wrap: wrap; */
+  grid: 10px;
+  border: 1px solid rgba(0, 0, 0, 0.05);
+}
+.img-show {
+  width: 80px;
+  height: 48px;
+  padding: 10px;
+  position: relative;
+}
+.img-item {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.img-detail {
+  width: 340px;
+  height: 340px;
+  border-radius: 4px;
+  border: 1px solid #c2ccd9;
+  background: #fff;
+  position: absolute;
+  top: 0px;
+  left: 80px;
+  z-index: 1;
+  padding: 20px;
+}
+.img-detail-title {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.img-detail-title-img {
+  width: 120px;
+  height: 40px;
+  object-fit: cover;
+}
+.img-detail-title-text {
+  margin-top: 20px;
+  margin-bottom: 10px;
+  color: #000;
+  font-family: "PingFang HK";
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 1.4px;
+}
+.img-detail-title-country {
+  color: #000;
+  font-family: "PingFang HK";
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 1.2px;
+  opacity: 0.5;
+}
+.img-detail-title-desc {
+  margin: 20px 0;
+  color: #000;
+  font-family: "PingFang HK";
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 1.4px;
+}
+.img-detail-bottom-desc {
+  display: flex;
+}
+.img-detail-bottom-desc-one {
+  color: rgba(0, 0, 0, 0.5);
+  font-family: "PingFang HK";
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 1.2px;
+  margin-right: 20px;
+}
+.img-detail-bottom-desc-span {
+  color: #000;
+  margin-left: 8px;
+}
+.img-detail-bottom-desc-web {
+  color: #000;
+  font-family: Avenir;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 1.2px;
+  text-decoration-line: underline;
+  text-decoration-style: solid;
+  text-decoration-skip-ink: auto;
+  text-decoration-thickness: auto;
+  text-underline-offset: auto;
+  text-underline-position: from-font;
+  display: flex;
+}
+.img-close:hover {
+  cursor: pointer;
+}
+.img-detail-bottom-desc-web:hover {
+  cursor: pointer;
+  color: #d91b64;
+  text-decoration-color: #d91b64;
 }
 </style>
